8101a2d9bd5ec506f7a70948a70a197272f807af
mcarri355 <carrillomatisa@gmail.com>
2022-09-09 17:14:22 -0700

    conversion js and css

--- conversion.js
+++ conversion.js
@@ -1,3 +1,13 @@
-var a = prompt(`Give a random number`)
+var a = prompt(`Give a number in base 10`)
 a = Number(a)
 
+
+// Converts deciaml to hexadecimal
+    console.log(a.toString(16))
+
+// Converts Decimal to binary and formats the binary
+function addLeadingZeros(num, totalLength) {
+  return String(num).padStart(totalLength, '0');
+}
+
+console.log(addLeadingZeros(a.toString(2), 8))
